9e57f980aa087fa7d38fa59ae1676941ab9e0fdc
zhangzhenfly <1445603877@qq.com>
2018-06-14 13:48:10 +0800

    four

---
+++ myfirstScript.js
@@ -0,0 +1,44 @@
+
+/*document.getElementById("skinone").onclick = function(){
+	
+	var oL=document.getElementById("l")
+	oL.href="style_1.css";
+}
+document.getElementById("skintwo").onclick = function(){
+	
+	var oL=document.getElementById("l")
+	oL.href="style_2.css";
+}
+document.getElementById("skinthree").onclick = function(){
+	
+	var oL=document.getElementById("l")
+	oL.href="style_3.css";
+}*/
+function skinone()
+{
+     var oL=document.getElementById("l")
+	 oL.href="style_1.css";
+}
+function skintwo()
+{
+     var oL=document.getElementById("l")
+	 oL.href="style_2.css";
+}
+function skinthree()
+{
+     var oL=document.getElementById("l")
+	 oL.href="style_3.css";
+}
+function one()
+{
+	alert("Hello,my name is FreeFly,welcome to my web")
+}
+function two1()
+{
+	    var oL=document.getElementById("c")
+        oL.innerHTML="You are probably a single dog"
+}function two2()
+{
+	  
+   
+}
